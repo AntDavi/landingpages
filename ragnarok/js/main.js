36205d7ea345036085590c7d478d70763e360d73
Anthony Davi <ant.davi07@gmail.com>
2022-11-27 15:49:42 -0300

    -ragnarok carrossel e animacao scroll

---
+++ ragnarok/js/main.js
@@ -0,0 +1,25 @@
+AOS.init({
+    easing: "ease-out-quart",
+    duration: 1000,
+    once: true,
+});
+
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    freeMode: true,
+    breakpoints: {
+        320: { //da resolução de 320px pra cima acontece algo
+            slidesPerView: 1.1,
+        },
+        768: {
+            slidesPerView: 2.2,
+        },
+        991: {
+            slidesPerView: 2.8,
+        },
+        1200: {
+            slidesPerView: 3.5,
+        }
+    }
+});
