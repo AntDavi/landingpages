f53f40ab72084d4c2cc5a9a3d4dfd175818c4d05
Anthony Davi <ant.davi07@gmail.com>
2023-01-01 18:21:05 -0300

    -neon configuracao inicial

--- weather.io/script.js
+++ weather.io/script.js
@@ -1,54 +1,57 @@
- const apiKey = "d102ce6f8a7f8c61a416505fdeb98697";
+const apiKey = "d102ce6f8a7f8c61a416505fdeb98697";
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-  
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d102ce6f8a7f8c61a416505fdeb98697`; 
+
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d102ce6f8a7f8c61a416505fdeb98697`;
 
 
-async function getWeatherByLocation(city){
-     
-         const resp = await fetch(url(city), {
-             origin: "cros" });
-         const respData = await resp.json();
-     
-           addWeatherToPage(respData);
-          
-     }
+async function getWeatherByLocation(city) {
 
-      function addWeatherToPage(data){
-          const temp = Ktoc(data.main.temp);
+    const resp = await fetch(url(city), {
+        origin: "cros"
+    });
+    const respData = await resp.json();
 
-          const weather = document.createElement('div')
-          weather.classList.add('weather');
+    addWeatherToPage(respData);
 
-          weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-          <small>${data.weather[0].main}</small>
-          
-          `;
+}
 
+function addWeatherToPage(data) {
+    const temp = Ktoc(data.main.temp);
 
-        //   cleanup 
-          main.innerHTML= "";
-           main.appendChild(weather);
-      };
+    const weather = document.createElement('div')
+    weather.classList.add('weather');
+
+    weather.innerHTML = `
+        <h2>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/> 
+        ${temp}°C 
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/></h2>
+        <small>${data.weather[0].main}</small>
+    `;
 
 
-     function Ktoc(K){
-         return Math.floor(K - 273.15);
-     }
+    //   cleanup 
+    main.innerHTML = "";
+    main.appendChild(weather);
+};
+
+
+function Ktoc(K) {
+    return Math.floor(K - 273.15);
+}
 
 
 
-     form.addEventListener('submit',(e) =>{
-        e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-        const city = search.value;
+    const city = search.value;
 
-        if(city){
-            getWeatherByLocation(city)
-        }
+    if (city) {
+        getWeatherByLocation(city)
+    }
 
-     });
+});
